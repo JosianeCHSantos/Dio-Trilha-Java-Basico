cf3fc78a43be454726b8f34bbee9eb01a80bcf2b
Josiane Cristina Henrique dos Santos <jacphs1326@outlook.com>
2022-12-05 16:10:34 -0300

    tipos-variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //estudar = Tipos Primivos
+        //estudar = classe String que representa texto na aplicação
+
+        String meuNome = "Josiane Santos";
+
+        double salarioMinimo = 2500,
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14; //não pode ser alterado
+
+
+
     }
 }
